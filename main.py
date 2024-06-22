f54d2cbc802ad16eb04d2eeecb62f09fe2060c51
Shraddha Acharya <acharyashradha71@gmail.com>
2024-06-22 22:41:47 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.mydb
+students = db.students
+print(db)
+# To insert
+students_1 = {
+    'Name': 'Jenny',
+    'Address': 'Paris',
+    'Course': 'Bsc.CSIT'
+    
+}
+students_2 = {
+      'Name': 'Merry',
+    'Address': 'London',
+    'Course': 'Psychology'
+  } 
+students.insert_one(students_1)
+students.insert_one(students_2)
+
+# To read a data
+students_1 = students.find_one()
+print(students_1)
+
+# To update
+students.update_one({"Name": "Jenny"}, {"$set": {"Course": "Psychology"}})
+students_1 = students.find()
+print(list(students_1))
+
+# To delete
+students.delete_one({"Name": "Merry"})
+students_1 = students.find()
+print(list(students_1))
+
